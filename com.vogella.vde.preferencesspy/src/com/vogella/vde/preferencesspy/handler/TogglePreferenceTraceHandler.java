8137dbce4f91d55037d8d8d9e294e726344ff9d2
Simon Scholz <simon.scholz@vogella.com>
2014-11-15 02:54:08 +0100

    Added logger in case a BackingStoreException occurs
    Change-Id: I3c95ac6f5798ff9d45f8e25c0433e3af5319ab9e
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.vde.preferencesspy/src/com/vogella/vde/preferencesspy/handler/TogglePreferenceTraceHandler.java
+++ com.vogella.vde.preferencesspy/src/com/vogella/vde/preferencesspy/handler/TogglePreferenceTraceHandler.java
@@ -3,14 +3,23 @@ package com.vogella.vde.preferencesspy.handler;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.extensions.Preference;
+import org.eclipse.e4.core.services.log.Logger;
+import org.osgi.service.prefs.BackingStoreException;
 
 import com.vogella.vde.preferencesspy.constants.PreferenceConstants;
 
 public class TogglePreferenceTraceHandler {
 	@Execute
-	public void execute(@Preference IEclipsePreferences eclipsePreferences) {
+	public void execute(@Preference IEclipsePreferences eclipsePreferences,
+			Logger log) {
 		eclipsePreferences.putBoolean(PreferenceConstants.TRACE_PREFERENCES,
 				!eclipsePreferences.getBoolean(
 						PreferenceConstants.TRACE_PREFERENCES, false));
+
+		try {
+			eclipsePreferences.flush();
+		} catch (BackingStoreException e) {
+			log.error(e);
+		}
 	}
 }
