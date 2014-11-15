531a4b3bfae5dc71f47527ba4d5e40b7d4706ea4
Simon Scholz <simon.scholz@vogella.com>
2014-11-15 04:13:45 +0100

    Added handling for toggling preferencetrace in the view
    Change-Id: I524f8c8226d5d8322638d2ddac8d7bbf96bd33c3
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.vde.preferencesspy/src/com/vogella/vde/preferencesspy/handler/TogglePreferenceTraceHandler.java
+++ com.vogella.vde.preferencesspy/src/com/vogella/vde/preferencesspy/handler/TogglePreferenceTraceHandler.java
@@ -4,6 +4,7 @@ import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.core.services.log.Logger;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolItem;
 import org.osgi.service.prefs.BackingStoreException;
 
 import com.vogella.vde.preferencesspy.constants.PreferenceConstants;
@@ -11,10 +12,15 @@ import com.vogella.vde.preferencesspy.constants.PreferenceConstants;
 public class TogglePreferenceTraceHandler {
 	@Execute
 	public void execute(@Preference IEclipsePreferences eclipsePreferences,
-			Logger log) {
-		eclipsePreferences.putBoolean(PreferenceConstants.TRACE_PREFERENCES,
-				!eclipsePreferences.getBoolean(
-						PreferenceConstants.TRACE_PREFERENCES, false));
+			MToolItem toolItem, Logger log) {
+		boolean selected = toolItem.isSelected();
+		boolean tracePreferences = eclipsePreferences.getBoolean(
+				PreferenceConstants.TRACE_PREFERENCES, false);
+
+		if (selected != tracePreferences) {
+			eclipsePreferences.putBoolean(
+					PreferenceConstants.TRACE_PREFERENCES, selected);
+		}
 
 		try {
 			eclipsePreferences.flush();
--- com.vogella.vde.preferencesspy/src/com/vogella/vde/preferencesspy/parts/PreferenceSpyPart.java
+++ com.vogella.vde.preferencesspy/src/com/vogella/vde/preferencesspy/parts/PreferenceSpyPart.java
@@ -1,5 +1,8 @@
 package com.vogella.vde.preferencesspy.parts;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
@@ -8,9 +11,16 @@ import org.eclipse.core.databinding.observable.list.WritableList;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences.PreferenceChangeEvent;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 import org.eclipse.jface.databinding.viewers.ViewerSupport;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
@@ -27,7 +37,9 @@ public class PreferenceSpyPart {
 	private WritableList input;
 
 	@PostConstruct
-	public void postConstruct(Composite parent) {
+	public void postConstruct(Composite parent,
+			final ESelectionService selectionService,
+			EModelService modelService, MWindow window) {
 		tableViewer = new TableViewer(parent);
 
 		Table table = tableViewer.getTable();
@@ -35,6 +47,20 @@ public class PreferenceSpyPart {
 		table.setLinesVisible(true);
 
 		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
+		tableViewer
+				.addSelectionChangedListener(new ISelectionChangedListener() {
+
+					@Override
+					public void selectionChanged(SelectionChangedEvent event) {
+						ISelection selection = event.getSelection();
+						if (selection instanceof IStructuredSelection) {
+							@SuppressWarnings("unchecked")
+							ArrayList<PreferenceEntry> preferenceEntries = new ArrayList<PreferenceEntry>(
+									((IStructuredSelection) selection).toList());
+							selectionService.setSelection(preferenceEntries);
+						}
+					}
+				});
 
 		createColumn(Fields.nodePath, "Nodepath", 300);
 		createColumn(Fields.key, "Key", 300);
@@ -72,15 +98,18 @@ public class PreferenceSpyPart {
 	@Inject
 	@Optional
 	public void DeletePreferenceEntries(
-			@UIEventTopic(PreferenceSpyEventTopics.PREFERENCESPY_PREFERENCE_ENTRIES_DELETE) PreferenceChangeEvent event) {
-		input.removeAll(input);
+			@UIEventTopic(PreferenceSpyEventTopics.PREFERENCESPY_PREFERENCE_ENTRIES_DELETE) List<PreferenceEntry> preferenceEntries) {
+		if (preferenceEntries != null && !preferenceEntries.isEmpty()) {
+			input.removeAll(preferenceEntries);
+		}
 	}
 
 	@Inject
-	@Optional
 	public void DeleteAllPreferenceEntries(
-			@UIEventTopic(PreferenceSpyEventTopics.PREFERENCESPY_PREFERENCE_ENTRIES_DELETE_ALL) PreferenceChangeEvent event) {
-		input.clear();
+			@Optional @UIEventTopic(PreferenceSpyEventTopics.PREFERENCESPY_PREFERENCE_ENTRIES_DELETE_ALL) List<PreferenceEntry> preferenceEntries) {
+		if (input != null) {
+			input.clear();
+		}
 	}
 
 }
