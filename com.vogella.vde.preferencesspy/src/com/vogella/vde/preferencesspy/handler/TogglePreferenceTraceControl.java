3e3b86031f0eecff352d2e5dfebfc98fdb9a8551
Simon Scholz <simon.scholz@vogella.com>
2014-12-17 13:11:46 +0100

    ToolItem for tracepreference now creates its own toolbar instead of using the one from the MPart direclty
    Change-Id: I581d4f5398eca8cef576bf16a258c0727791e42d
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.vde.preferencesspy/src/com/vogella/vde/preferencesspy/handler/TogglePreferenceTraceControl.java
+++ com.vogella.vde.preferencesspy/src/com/vogella/vde/preferencesspy/handler/TogglePreferenceTraceControl.java
@@ -10,8 +10,6 @@ import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.e4.core.di.extensions.Preference;
-import org.eclipse.e4.ui.model.application.ui.basic.MPart;
-import org.eclipse.e4.ui.model.application.ui.menu.MToolBar;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.resource.LocalResourceManager;
@@ -19,6 +17,7 @@ import org.eclipse.jface.resource.ResourceManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 import org.osgi.framework.Bundle;
@@ -40,28 +39,23 @@ public class TogglePreferenceTraceControl {
 	}
 
 	@PostConstruct
-	public void createGui(MPart element, final @Preference IEclipsePreferences preferences,
+	public void createGui(Composite parent, final @Preference IEclipsePreferences preferences,
 			@Preference(value = PreferenceConstants.TRACE_PREFERENCES) boolean tracePreferences) {
-		MToolBar mToolbar = element.getToolbar();
-		Object widget = mToolbar.getWidget();
-		if (widget instanceof ToolBar) {
-			ToolBar toolBar = (ToolBar) widget;
-			toolItem = new ToolItem(toolBar, SWT.CHECK);
-			toolItem.setSelection(tracePreferences);
-			toolItem.setToolTipText("Toggle Preference Trace");
-			toolItem.setImage(getResourceManager().createImage(getImageDescriptor()));
-			toolItem.addSelectionListener(new SelectionAdapter() {
+		ToolBar toolBar = new ToolBar(parent, SWT.NONE);
+		toolItem = new ToolItem(toolBar, SWT.CHECK);
+		toolItem.setSelection(tracePreferences);
+		toolItem.setToolTipText("Toggle Preference Trace");
+		toolItem.setImage(getResourceManager().createImage(getImageDescriptor()));
+		toolItem.addSelectionListener(new SelectionAdapter() {
 
-				@Override
-				public void widgetSelected(SelectionEvent e) {
-					Object source = e.getSource();
-					if (source instanceof ToolItem) {
-						preferences.putBoolean(PreferenceConstants.TRACE_PREFERENCES,
-								((ToolItem) source).getSelection());
-					}
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				Object source = e.getSource();
+				if (source instanceof ToolItem) {
+					preferences.putBoolean(PreferenceConstants.TRACE_PREFERENCES, ((ToolItem) source).getSelection());
 				}
-			});
-		}
+			}
+		});
 	}
 
 	@PreDestroy
